5071827a9c073af55391d793836c0fb6fc16a7df
Eli <elicloudh@gmail.com>
2018-07-07 03:16:22 -0400

    Admin only allowed to be in admin zone

--- app/controllers/statics_controller.rb
+++ app/controllers/statics_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class StaticsController < ApplicationController
+  before_action { redirect_to admin_orders_path if current_user&.admin? }
   def index; end
 
   def how_it_works;end
